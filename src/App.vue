23bef64fff72b19796cc5fcae271bc6bdb1215e4
Guilherme <guilhermeerba@hotmail.com>
2021-02-11 23:01:12 -0300

    create base of project and add requests to API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <div class="container">
+        <h1>Navita</h1>
+      </div>
+    </header>
+    
+    <CarsList />
+
+    <footer>
+      <div class="container">
+        <p>Copyright © Navita 2020</p>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CarsList from './components/CarsList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CarsList
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './styles/reset.css';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #5A5C69;
+}
+
+body {
+  background: #F7F8FB;
+}
+
+.container {
+  width: 1140px;
+  margin: 0 auto;
+}
+
+header {
+  width: 100%;
+  background-color:#FFFFFF;
+  padding: 20px 0;
+  box-shadow: 0 4px 2px -2px rgba(58, 59, 69, .15);
+  
+  h1 {
+    color: #1CC88A;
+    font-weight: bold;
+    font-size: 26px;
+  }
+}
+
+footer {
+  width: 100%;
+  background-color:#FFFFFF;
+  padding: 40px 0;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 50px;
+}
+
+/* ================= Responsive ================= */
+@media screen and (max-width: 768px) {
+  .container {
+    width: 100%;
+  }
+
+  header {
+    h1 {
+      padding-left: 12px;
+    }
+  }
 }
 </style>
---
+++ src/components/CarsList.vue
@@ -0,0 +1,187 @@
+<template>
+  <main class="list-cars">
+    <div class="container">
+      <h2>Veículos</h2>
+
+      <section class="list-cars__brands-box">
+        <aside class="list-cars__brands-box--title">
+          <h3>Marcas</h3>
+        </aside>
+
+        <div class="list-cars__brands-box--infos">
+          <h4>Marca</h4>
+
+          <ul>
+            <li v-for="(car, index) in carsData" :key="index" class="list-cars__box-infos--car">
+              <p>{{car.nome}}</p>
+
+              <button :class="{ active: index === activeItem}" @click.prevent="getModelsCars(car.codigo, index)">Ver modelos</button>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="list-cars__models-box" v-if="modelsData">
+        <aside class="list-cars__models-box--title">
+          <h3>Modelos</h3>
+        </aside>
+
+        <div class="list-cars__models-box--infos">
+          <h4>Modelo</h4>
+
+          <ul>
+            <li v-for="(model, index) in modelsData" :key="index" class="list-cars__box-infos--car">
+              <p>{{model.nome}}</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+import { api } from "@/services.js";
+
+export default {
+  name: 'CarsList',
+  data() {
+    return {
+      carsData: null,
+      modelsData: null,
+      activeItem: null,
+    };
+  },
+  methods: {
+    getBrandsCars() {
+      api.get(`/carros/marcas`).then(r => {
+        this.carsData = r.data;
+      });
+    },
+    getModelsCars(idCar, index) {
+      this.activeItem = index;
+
+      api.get(`/carros/marcas/${idCar}/modelos`).then(r => {
+        this.modelsData = r.data.modelos;
+      });
+    },
+  },
+  created() {
+    this.getBrandsCars();
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.list-cars {
+  h2 {
+    font-weight: bold;
+    font-size: 32px;
+    padding: 20px 0;
+  }
+
+  &__models-box {
+    margin-top: 50px;
+  }
+
+  &__brands-box,
+  &__models-box {
+    border-radius: 5px;
+    background-color: #FFFFFF;
+    box-shadow: 0px 1px 14px 5px rgba(58, 59, 69, .15);
+
+     &--title {
+       border-top-left-radius: 5px;
+       border-top-right-radius: 5px;
+       background-color:#F8F9FC;
+       padding: 15px 20px;
+       border-bottom: 1px solid #E3E6F0;
+
+       h3 {
+         font-size: 22px;
+         font-weight: bold;
+       }
+     }
+
+     &--infos {
+       width: 95%;
+       margin: 20px auto;
+       padding: 0 15px;
+       
+
+       h4 {
+         border-top: 1px solid #E3E6F0;
+         border-bottom: 2px solid #E3E6F0;
+         padding: 15px 0;
+         font-size: 20px;
+         font-weight: bold;
+         color: #4E73DF;
+       }
+
+       ul {
+         max-height: 450px;
+         overflow: auto;
+       }
+
+       li {
+         padding: 20px 0 30px 0;
+         border-bottom: 1px solid #E3E6F0;
+         display: flex;
+         position: relative;
+         width: 100%;
+
+         p {
+           font-size: 18px;
+         }
+
+         button {
+           border: 0;
+           background: none;
+           color: #4E73DF;
+           font-weight: bold;
+           left: 50%;
+           position: absolute;
+           transition: 0.2s;
+
+           &.active {
+            color: #1CC88A;
+           }
+         }
+       }
+     }
+  }
+}
+
+/* ================= Responsive ================= */
+@media screen and (max-width: 768px) {
+  .list-cars {
+    h2 {
+      font-size: 28px;
+      padding-left: 12px;
+    }
+    
+    &__brands-box,
+    &__models-box {
+      margin: 0 5px;
+
+      &--title {
+        h3 {
+          font-size: 18px;
+       }
+      }
+
+      &--infos {
+        h4 {
+         font-size: 18px;
+       }
+
+       li {
+         p {
+           font-size: 16px;
+         }
+       }
+      }
+    }
+  }
+}
+</style>
